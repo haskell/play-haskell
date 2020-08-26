39b31750e8741ff2d999bb9963b1482b44bbb444
Tom Smeding <tom.smeding@gmail.com>
2020-08-26 22:26:06 +0200

    Use SQLite instead of in-memory store

---
+++ DB.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+module DB (
+    Database, ErrCode(..), KeyType, ContentsType,
+    withDatabase,
+    storePaste, getPaste
+) where
+
+import Control.Exception (tryJust, handleJust)
+import Control.Monad (when)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Short as Short
+import qualified Data.Text as T
+import Database.SQLite.Simple
+import System.Exit (die)
+import System.IO (hPutStrLn, stderr)
+
+
+maxDbFileSize :: Int
+maxDbFileSize = 1024 * 1024 * 1024  -- 1 GiB
+
+dbFileName :: FilePath
+dbFileName = "pastes.db"
+
+
+newtype Database = Database Connection
+
+type KeyType = Short.ShortByteString
+type ContentsType = ByteString
+
+data ErrCode = ErrExists  -- ^ Key already exists in database
+             | ErrFull    -- ^ Database disk quota has been reached
+  deriving (Show)
+
+withDatabase :: (Database -> IO ()) -> IO ()
+withDatabase act =
+    withConnection dbFileName $ \conn -> do
+        execute_ conn "PRAGMA foreign_keys = on"
+        [Only pageSize] <- query_ conn "PRAGMA page_size"
+        [Only pageCount] <- query_ conn "PRAGMA page_count"
+
+        let maxPages = max pageCount (maxDbFileSize `div` pageSize)
+        when (pageCount > maxDbFileSize `div` pageSize) $
+            hPutStrLn stderr "WARNING: database is already larger than limit!"
+
+        databaseVersion (Database conn) >>= \case
+            Nothing -> do
+                applySchema (Database conn)
+                hPutStrLn stderr $ "Created database at '" ++ dbFileName ++ "'"
+            Just ver | ver == schemaVersion -> return ()
+                     | otherwise -> die $ "ERROR: Incompatible database schema version (DB " ++
+                                            show ver ++ ", app " ++ show schemaVersion ++ ")"
+
+        -- SQLite can't prepare a PRAGMA statement apparently, so let's do it
+        -- like this (acceptable because this is, statically, an Int)
+        execute_ conn $ Query (T.pack ("PRAGMA max_page_count = " ++ show maxPages))
+        act (Database conn)
+
+schema :: [Query]
+schemaVersion :: Int
+(schema, schemaVersion) =
+    (["CREATE TABLE meta (version INTEGER);"
+     ,"CREATE TABLE pastes (key BLOB PRIMARY KEY, value BLOB);\n"]
+    ,1)
+
+databaseVersion :: Database -> IO (Maybe Int)
+databaseVersion (Database conn) = do
+    let predicate (SQLError { sqlError = ErrorError }) = Just ()
+        predicate _ = Nothing
+    res <- tryJust predicate $ query_ conn "SELECT version FROM meta"
+    case res of
+        Right [Only ver] -> return (Just ver)
+        Right [] -> die "ERROR: no version field in database meta table"
+        Right _ -> die "ERROR: stop fooling around in meta"
+        Left () -> return Nothing
+
+applySchema :: Database -> IO ()
+applySchema (Database conn) = do
+    mapM_ (execute_ conn) schema
+    execute conn "INSERT INTO meta (version) VALUES (?)" (Only schemaVersion)
+
+storePaste :: Database -> Short.ShortByteString -> ByteString -> IO (Maybe ErrCode)
+storePaste (Database conn) key value =
+    let predicate (SQLError { sqlError = ErrorError }) = Just ()
+        predicate _ = Nothing
+    in handleJust predicate (const (return (Just ErrFull))) $
+        withTransaction conn $ do
+            [Only count] <- query conn "SELECT COUNT(*) FROM pastes WHERE key = ?"
+                                       (Only (Short.fromShort key))
+            if (count :: Int) == 0
+                then do
+                    execute conn "INSERT INTO pastes (key, value) VALUES (?, ?)"
+                                 (Short.fromShort key, value)
+                    return Nothing
+                else return (Just ErrExists)
+
+getPaste :: Database -> Short.ShortByteString -> IO (Maybe ByteString)
+getPaste (Database conn) key = do
+    res <- query conn "SELECT value FROM pastes WHERE key = ?" (Only (Short.fromShort key))
+    case res of
+        [Only value] -> return (Just value)
+        [] -> return Nothing
+        _ -> return Nothing  -- multiple entries, what?
--- Main.hs
+++ Main.hs
@@ -1,10 +1,7 @@
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE TypeApplications #-}
 module Main (main) where
 
-import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM
-import Control.Exception (finally)
 import Control.Monad
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as BS
@@ -14,23 +11,16 @@ import qualified Data.ByteString.Short as Short
 import qualified Data.ByteString.Char8 as Char8
 import Data.ByteString (ByteString)
 import Data.Char
-import Data.Foldable (toList)
-import Data.IORef
-import qualified Data.Map.Strict as Map
-import Data.Map.Strict (Map)
-import qualified Data.Sequence as Seq
-import Data.Sequence (Seq)
+import Data.String (fromString)
 import Snap.Core hiding (path, method)
 import Snap.Http.Server
-import System.Environment (getArgs)
-import System.Exit (die)
 import System.IO
 import qualified System.Posix.Signals as Signal
 import System.Random
 
-import Mutex
+import qualified DB
+import DB (Database, KeyType, ContentsType)
 import Pages
-import Persist
 
 
 htmlEscape :: ByteString -> Builder.Builder
@@ -47,9 +37,6 @@ alphabet = Char8.pack (['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'])
 minKeyLength, maxKeyLength :: Int
 (minKeyLength, maxKeyLength) = (8, 8)
 
-maxMemoryUsage :: Int
-maxMemoryUsage = 128 * 1024 * 1024
-
 maxPasteSize :: Int
 maxPasteSize = 64 * 1024
 
@@ -63,94 +50,49 @@ genKey gen =
                 $ bs
     in (bs', gen')
 
-genKeySatisfying :: StdGen -> (KeyType -> Bool) -> (KeyType, StdGen)
-genKeySatisfying gen predicate =
-    let (key, gen') = genKey gen
-    in if predicate key then (key, gen') else genKey gen'
-
 type AtomicState = TVar State
-type KeyType = Short.ShortByteString
-type ContentsType = ByteString
+
+data Context = Context { cDB :: Database }
 
 data State = State
-    { sPasteMap :: Map KeyType ContentsType
-    , sHistory :: Seq KeyType
-    , sRandGen :: StdGen
-    , sTotalSize :: Int
-    , sPages :: Pages
-    , sModifyCookie :: Int }
+    { sRandGen :: StdGen
+    , sPages :: Pages }
 
-newState :: StdGen -> Pages -> [(Short.ShortByteString, ByteString)] -> State
-newState randgen pages persistedPastes =
-    State { sPasteMap = Map.fromList persistedPastes
-          , sHistory = Seq.fromList (map fst persistedPastes)
-          , sRandGen = randgen
-          , sTotalSize = sum (map (BS.length . snd) persistedPastes)
-          , sPages = pages
-          , sModifyCookie = 0 }
+newState :: StdGen -> Pages -> State
+newState randgen pages =
+    State { sRandGen = randgen
+          , sPages = pages }
 
-stateGetPage :: (Pages -> a) -> IORef AtomicState -> IO a
-stateGetPage f stref =
-    f . sPages <$> (readIORef stref >>= atomically . readTVar)
+genKey' :: AtomicState -> IO KeyType
+genKey' var = atomically $ do
+    state <- readTVar var
+    let (key, gen') = genKey (sRandGen state)
+    writeTVar var (state { sRandGen = gen' })
+    return key
 
-sizeOverheadPerPaste :: Int
-sizeOverheadPerPaste =
-    -- These are complete guesstimates!
-    -- 3*keysize bytes overhead for history
-    -- keysize bytes contents for history
-    -- 3*keysize bytes overhead for pasteMap
-    -- keysize bytes contents for pasteMap
-    8 * maxKeyLength
+stateGetPage :: (Pages -> a) -> AtomicState -> IO a
+stateGetPage f stvar =
+    f . sPages <$> atomically (readTVar stvar)
 
-purgeOldPastes :: State -> State
-purgeOldPastes state
-  | sTotalSize state + sizeOverheadPerPaste * Map.size (sPasteMap state)
-        > maxMemoryUsage
-  , old Seq.:<| rest <- sHistory state
-  = let oldLength = maybe 0 BS.length (Map.lookup old (sPasteMap state))
-    in purgeOldPastes $
-        state { sPasteMap = Map.delete old (sPasteMap state)
-              , sHistory = rest
-              , sTotalSize = sTotalSize state - oldLength
-              , sModifyCookie = sModifyCookie state + 1 }
-  | otherwise
-  = state
+-- returns the generated key, or an error string
+genStorePaste :: Context -> AtomicState -> ContentsType -> IO (Either String KeyType)
+genStorePaste context stvar contents =
+    let loop iter = do
+            key <- genKey' stvar
+            DB.storePaste (cDB context) key contents >>= \case
+                Nothing -> return (Right key)
+                Just DB.ErrExists
+                    | iter < 5 -> loop (iter + 1)  -- try again with a new key
+                    | otherwise -> return (Left "Database full?")
+                Just DB.ErrFull -> return (Left "Too many pastes submitted, please notify tomsmeding")
+    in loop (0 :: Int)
 
-storePaste :: IORef AtomicState -> ContentsType -> IO KeyType
-storePaste stref contents = do
-    var <- readIORef stref
-    atomically $ do
-        state@State { sPasteMap = mp } <- readTVar var
-        let (key, gen') = genKeySatisfying (sRandGen state) (`Map.notMember` mp)
-            mp' = Map.insert key contents mp
-            state' = state { sPasteMap = mp'
-                           , sRandGen = gen'
-                           , sHistory = sHistory state Seq.|> key
-                           , sTotalSize = sTotalSize state + BS.length contents
-                           , sModifyCookie = sModifyCookie state + 1 }
-            state'' = purgeOldPastes state'
-        writeTVar var state''
-        return key
+getPaste :: Context -> Short.ShortByteString -> IO (Maybe ByteString)
+getPaste context = DB.getPaste (cDB context)
 
-getPaste :: IORef AtomicState -> KeyType -> IO (Maybe ContentsType)
-getPaste stref key = do
-    var <- readIORef stref
-    atomically $ Map.lookup key . sPasteMap <$> readTVar var
-
-diagnostics :: IORef AtomicState -> IO ()
-diagnostics stref = do
-    state <- readIORef stref >>= (atomically . readTVar)
-    putStrLn $
-        "diagnostics: "
-            ++ show (Map.size (sPasteMap state)) ++ " pastes, "
-            ++ show (sTotalSize state + Map.size (sPasteMap state) * sizeOverheadPerPaste) ++ " memory used"
-            -- ++ ": " ++ intercalate " " (map (Char8.unpack . Short.fromShort)
-            --                                 (toList (sHistory state)))
-    return ()
-
-pasteReadResponse :: IORef AtomicState -> KeyType -> ContentsType -> IO Lazy.ByteString
-pasteReadResponse stref key contents = do
-    (pre, mid, post) <- stateGetPage pPasteRead stref
+pasteReadResponse :: AtomicState -> KeyType -> ContentsType -> IO Lazy.ByteString
+pasteReadResponse stvar key contents = do
+    (pre, mid, post) <- stateGetPage pPasteRead stvar
     return $ Builder.toLazyByteString $ mconcat
         [Builder.byteString pre
         ,Builder.shortByteString key
@@ -166,82 +108,61 @@ parsePasteGet bs = do
     guard (fromIntegral (ord '/') `BS.notElem` key)
     return (Short.toShort key)
 
-error400 :: String -> Snap ()
-error400 msg = do
-    putResponse $ setResponseCode 400 emptyResponse
-    writeBS (Char8.pack msg)
-
-error404 :: String -> Snap ()
-error404 msg = do
-    putResponse $ setResponseCode 404 emptyResponse
+httpError :: Int -> String -> Snap ()
+httpError code msg = do
+    putResponse $ setResponseCode code emptyResponse
     writeBS (Char8.pack msg)
 
 staticFile :: String -> FilePath -> Snap ()
 staticFile mime path = do
-    modifyResponse $ setContentType (Char8.pack mime)
+    modifyResponse $
+        setContentType (Char8.pack mime)
+        . setHeader (fromString "Cache-Control") (Char8.pack "public max-age=3600")
     sendFile path
 
-handleRequest :: IORef AtomicState -> Method -> ByteString -> Snap ()
-handleRequest stref GET path
+handleRequest :: Context -> AtomicState -> Method -> ByteString -> Snap ()
+handleRequest context stvar GET path
   | path == Char8.pack "/" = do
-      liftIO (stateGetPage pIndex stref) >>= writeBS
+      liftIO (stateGetPage pIndex stvar) >>= writeBS
   | path == Char8.pack "/highlight.pack.js" = staticFile "text/javascript" "highlight.pack.js"
   | path == Char8.pack "/highlight.pack.css" = staticFile "text/css" "highlight.pack.css"
   | BS.take 7 path == Char8.pack "/paste/" =
       redirect' (BS.drop 6 path) 301  -- moved permanently
   | Just key <- parsePasteGet path = do
-      res <- liftIO $ getPaste stref key
+      res <- liftIO $ getPaste context key
       case res of
-          Just contents -> liftIO (pasteReadResponse stref key contents) >>= writeLBS
-          Nothing -> error404 "Paste not found"
-handleRequest stref POST path
+          Just contents -> liftIO (pasteReadResponse stvar key contents) >>= writeLBS
+          Nothing -> httpError 404 "Paste not found"
+handleRequest context stvar POST path
   | path == Char8.pack "/paste" = do
       mbody <- rqPostParam (Char8.pack "code") <$> getRequest
       case mbody of
           Just [body]
             | BS.length body <= maxPasteSize -> do
-                key <- liftIO $ storePaste stref body
-                liftIO $ diagnostics stref
-                redirect' (Char8.pack "/" `BS.append` Short.fromShort key) 303 -- see other
+                mkey <- liftIO $ genStorePaste context stvar body
+                case mkey of
+                    Right key -> redirect' (Char8.pack "/" `BS.append` Short.fromShort key) 303 -- see other
+                    Left err -> httpError 500 err
             | otherwise -> do
-                error400 "Paste too large"
-          Just _ -> error400 "Multiple code parameters given"
-          Nothing -> error400 "No paste given"
-handleRequest _ _ _ = error404 "Page not found"
+                httpError 400 "Paste too large"
+          Just _ -> httpError 400 "Multiple code parameters given"
+          Nothing -> httpError 400 "No paste given"
+handleRequest _ _ _ _ = httpError 404 "Page not found"
 
-persistToDisk :: State -> IO ()
-persistToDisk state = do
-    let pastes = [(key, sPasteMap state Map.! key)
-                 | key <- toList (sHistory state)]
-    Lazy.writeFile "pastes.txt" $
-        Builder.toLazyByteString (persist pastes)
-
-installPageReloader :: IORef AtomicState -> IO ()
-installPageReloader stref = do
+installPageReloader :: AtomicState -> IO ()
+installPageReloader stvar = do
     let handler = do
             pages <- pagesFromDisk
-            var <- readIORef stref
-            atomically $ modifyTVar var $ \state -> state { sPages = pages }
+            atomically $ modifyTVar stvar $ \state -> state { sPages = pages }
             putStrLn "Reloaded pages"
     void $ Signal.installHandler Signal.sigUSR1 (Signal.Catch handler) Nothing
 
-startPersistThread :: IORef AtomicState -> Mutex -> IO ()
-startPersistThread stref persistMutex = void $ forkIO $ loop (-1)
-  where
-    loop prevCookie = do
-        threadDelay (3 * 3600 * 1000 * 1000)
-        state <- readIORef stref >>= atomically . readTVar
-        let cookie = sModifyCookie state
-        when (cookie /= prevCookie) $
-            withLock persistMutex (persistToDisk state)
-        loop cookie
-
-server :: IORef AtomicState -> Snap ()
-server stref = do
+server :: Context -> AtomicState -> Snap ()
+server context stvar = do
     req <- getRequest
     let path = rqContextPath req `BS.append` rqPathInfo req
         method = rqMethod req
-    handleRequest stref method path
+    handleRequest context stvar method path
 
 config :: Config Snap a
 config =
@@ -252,34 +173,16 @@ config =
        $ defaultConfig
 
 main :: IO ()
-main = do
-    pasteList <- getArgs >>= \case
-        [fname] -> do
-            (list, rest) <- parse @[(Short.ShortByteString, ByteString)]
-                                <$> BS.readFile fname
-            when (BS.length rest /= 0) $ die $ "Cannot parse file '" ++ fname ++ "'"
-            return list
-        [] -> return []
-        _ -> die $ unlines $
-                ["Usage:"
-                ,"  ./pastebin-haskell"
-                ,"        -- Starts server from clean state"
-                ,"  ./pastebin-haskell <pastes.txt>"
-                ,"        -- Starts server with initial pastes from persisted file"]
+main = DB.withDatabase $ \db -> do
+    let context = Context db
 
     -- Create state
     randgen <- newStdGen
     pages <- pagesFromDisk
-    stref <- newTVarIO (newState randgen pages pasteList) >>= newIORef
-
-    persistMutex <- newMutex
+    stvar <- newTVarIO (newState randgen pages)
 
     -- Reload pages from disk on SIGUSR1
-    installPageReloader stref
-    -- Persist pastes to disk once in a while
-    startPersistThread stref persistMutex
+    installPageReloader stvar
 
-    -- Run server; after ^C perform an extra persist to disk
-    httpServe config (server stref) `finally`
-        (withLock persistMutex $
-            readIORef stref >>= atomically . readTVar >>= persistToDisk)
+    -- Run server
+    httpServe config (server context stvar)
