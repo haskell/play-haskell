1ddaed19ad1ac17467fb8ad4a7b623b107157ef8
Tom Smeding <tom.smeding@gmail.com>
2020-11-02 20:44:48 +0100

    Don't error out when serving a paste with invalid utf8

--- Pages.hs
+++ Pages.hs
@@ -9,6 +9,7 @@ import Data.ByteString (ByteString)
 import qualified Data.Text as Text
 import Data.Text (Text)
 import qualified Data.Text.Encoding as Enc
+import qualified Data.Text.Encoding.Error as Enc (lenientDecode)
 import Data.Time.Clock (nominalDiffTimeToSeconds)
 import Data.Time.Clock.POSIX (POSIXTime, posixSecondsToUTCTime)
 import System.Exit (die)
@@ -40,26 +41,26 @@ renderIndexPage templ (Contents [] mparent) =
 renderIndexPage templ (Contents files mparent) = Enc.encodeUtf8 $
     Mustache.substituteValue templ $ Mustache.object
         [(Text.pack "pastes", mixinPasteList files)
-        ,(Text.pack "parentAttr", mixinMaybeNull (escapeAttribute . Enc.decodeUtf8) mparent)]
+        ,(Text.pack "parentAttr", mixinMaybeNull (escapeAttribute . decodeUtf8) mparent)]
 
 renderReadPage :: Mustache.Template -> KeyType -> Maybe POSIXTime -> Contents -> ByteString
 renderReadPage templ key mdate (Contents files mparent) = Enc.encodeUtf8 $
     Mustache.substituteValue templ $ Mustache.object
-        [(Text.pack "key", toMustache (Enc.decodeUtf8 key))
+        [(Text.pack "key", toMustache (decodeUtf8 key))
         ,(Text.pack "date", mixinMaybeNull (show . posixSecondsToUTCTime) mdate)
         ,(Text.pack "unixdate", mixinMaybeNull @Int (truncate . nominalDiffTimeToSeconds) mdate)
         ,(Text.pack "pastes", mixinPasteList files)
-        ,(Text.pack "parent", mixinMaybeNull Enc.decodeUtf8 mparent)
-        ,(Text.pack "parentAttr", mixinMaybeNull (escapeAttribute . Enc.decodeUtf8) mparent)]
+        ,(Text.pack "parent", mixinMaybeNull decodeUtf8 mparent)
+        ,(Text.pack "parentAttr", mixinMaybeNull (escapeAttribute . decodeUtf8) mparent)]
 
 mixinPasteList :: [(Maybe ByteString, ByteString)] -> Mustache.Value
 mixinPasteList = toMustache . zipWith mixinSinglePaste [1..]
 
 mixinSinglePaste :: Int -> (Maybe ByteString, ByteString) -> Mustache.Value
 mixinSinglePaste index (mfname, contents) = Mustache.object $
-    [(Text.pack "fname", mixinMaybeNull Enc.decodeUtf8 mfname)
-    ,(Text.pack "fnameAttr", maybe (toMustache "") (toMustache . escapeAttribute . Enc.decodeUtf8) mfname)
-    ,(Text.pack "contents", toMustache (Enc.decodeUtf8 contents))
+    [(Text.pack "fname", mixinMaybeNull decodeUtf8 mfname)
+    ,(Text.pack "fnameAttr", maybe (toMustache "") (toMustache . escapeAttribute . decodeUtf8) mfname)
+    ,(Text.pack "contents", toMustache (decodeUtf8 contents))
     ,(Text.pack "index", toMustache index)
     ,(Text.pack "firstFile", toMustache (index == 1))]
 
@@ -78,3 +79,6 @@ escapeAttribute text
           '"' -> Text.pack "&quot;"
           c -> Text.singleton c
   | otherwise = text
+
+decodeUtf8 :: ByteString -> Text
+decodeUtf8 = Enc.decodeUtf8With Enc.lenientDecode
