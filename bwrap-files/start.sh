6b178167d7fd3e83f625ecf129a9a5d8599b8aa5
Julian Ospald <hasufell@posteo.de>
2022-03-26 00:36:41 +0100

    More restrictive bwrap

--- bwrap-files/start.sh
+++ bwrap-files/start.sh
@@ -7,23 +7,25 @@ cd "$filesdir"
 homedir="$1"
 
 args=(
-  --ro-bind /bin /bin
-  --ro-bind /usr/bin /usr/bin
+  --ro-bind /bin/bash /bin/bash
+  --ro-bind /bin/gcc /bin/gcc
+  --ro-bind /bin/dirname /bin/dirname
+  --ro-bind /bin/cat /bin/cat
   --ro-bind /usr/lib /usr/lib
   --ro-bind /lib /lib
   --ro-bind /lib64 /lib64
-  --ro-bind /etc /etc
   --ro-bind "${homedir}/.ghcup/bin" "${homedir}/.ghcup/bin"
   --ro-bind "${homedir}/.ghcup/ghc" "${homedir}/.ghcup/ghc"
+  --symlink bash /bin/sh
+  --setenv PATH "/bin"
   --tmpfs /tmp
-  --dev /dev
   --proc /proc
   --dir "${homedir}/workdir"
   --new-session
   --unshare-all
   --die-with-parent
   --file 4 "${homedir}/workdir/entry.sh"
-  bash "${homedir}/workdir/entry.sh"
+  /bin/bash "${homedir}/workdir/entry.sh"
 )
 
 exec bwrap "${args[@]}" 4<"${filesdir}/entry.sh"
