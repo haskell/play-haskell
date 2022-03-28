f872c47bed14b0a9fece03e972bf0a3cd49051cb
Tom Smeding <tom@tomsmeding.com>
2022-03-28 22:47:26 +0200

    Clean up entry.sh

--- bwrap-files/entry.sh
+++ bwrap-files/entry.sh
@@ -18,20 +18,24 @@ if ! ghcup --offline whereis ghc "${version}" 2>/tmp/null 1>/tmp/null ; then
 	exit 1
 fi
 
+function ghcup_run() {
+	ghcup --offline run --ghc "$version" -- "$@" 1>/tmp/ghc.out 2>/tmp/ghc.err || { err=$? ; cat /tmp/ghc.out ; cat /tmp/ghc.err >&2 ; exit $err ; }
+}
+
 case "$command" in
 	run)
 		cat >input.hs
-		ghcup --offline run --ghc "${version}" -- ghc -rtsopts -o Main "${opt}" input.hs 2>/tmp/ghc.err 1>/tmp/ghc.out || { err=$? ; >&2 cat /tmp/ghc.err ; cat /tmp/ghc.out ; exit $err ; } 
+		ghcup_run ghc -rtsopts -o Main "${opt}" input.hs
 		./Main +RTS -M100m -RTS
 		;;
 	core)
 		cat >input.hs
-		ghcup --offline run --ghc "${version}" -- ghc -ddump-simpl -ddump-to-file -o Main "${opt}" input.hs 2>/tmp/ghc.err 1>/tmp/ghc.out || { err=$? ; >&2 cat /tmp/ghc.err ; cat /tmp/ghc.out ; exit $err ; }
+		ghcup_run ghc -ddump-simpl -ddump-to-file -o Main "${opt}" input.hs
 		cat input.dump-simpl
 		;;
 	asm)
 		cat >input.hs
-		ghcup --offline run --ghc "${version}" -- ghc -ddump-asm -ddump-to-file -o Main "${opt}" input.hs 2>/tmp/ghc.err 1>/tmp/ghc.out || { err=$? ; >&2 cat /tmp/ghc.err ; cat /tmp/ghc.out ; exit $err ; }
+		ghcup_run ghc -ddump-asm -ddump-to-file -o Main "${opt}" input.hs
 		cat input.dump-asm
 		;;
 
