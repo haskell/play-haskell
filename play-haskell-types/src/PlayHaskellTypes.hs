af9142a69117c66843e4196c50985bdc39a7401e
Tom Smeding <tom@tomsmeding.com>
2022-04-30 16:49:18 +0200

    types: Signed messages

--- play-haskell-types/src/PlayHaskellTypes.hs
+++ play-haskell-types/src/PlayHaskellTypes.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS -Wno-orphans #-}
 module PlayHaskellTypes (
   -- * Supporting types
@@ -7,31 +10,63 @@ module PlayHaskellTypes (
   RunError(..),
 
   -- * Request/response types
+  Message(..),
+  signMessage,
   RunRequest(..),
   RunResponse(..),
+
+  -- * Signing utilities
+  SigningBytes(..),
 ) where
 
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.ByteString.Builder (Builder)
+import qualified Data.ByteString.Builder as BSB
+import qualified Data.ByteString.Lazy as Lazy
+import Data.Int (Int64)
+import Data.Word (Word64)
 import System.Exit
 import Text.JSON
 
+import PlayHaskellTypes.Sign (PublicKey, SecretKey, Signature)
+import qualified PlayHaskellTypes.Sign as Sign
 
+
+-- | JSON: string; "run", "core", "asm".
 data Command = CRun   -- ^ Compile and execute a Haskell file
              | CCore  -- ^ Get simplified core
              | CAsm   -- ^ Get native assembly
   deriving (Show)
 
+-- | JSON: string; \"O0", \"O1", \"O2".
 data Optimisation = O0 | O1 | O2
   deriving (Show, Read)
 
 -- | The GHC version to use for a job.
+--
+-- JSON: string.
 newtype Version = Version String
   deriving (Show)
 
+-- | JSON: string; "timeout".
 data RunError = RETimeOut
+  deriving (Show)
+
+-- | A signed message. The bytes being signed are the 'signingBytes' of the content.
+--
+-- JSON: object; "sig", "pkey", "con".
+data Message a = Message
+  { sesmsgSignature :: Signature  -- ^ Signature of the content with the secret key of the sender.
+  , sesmsgPublicKey :: PublicKey  -- ^ Public key of the sender.
+  , sesmsgContent :: a            -- ^ Message content.
+  }
   deriving (Show)
 
 -- | Body payload for a request to a worker for running a particular piece of
 -- source code.
+--
+-- JSON: object; "cmd", "src", "ver", "opt".
 data RunRequest = RunRequest
   { runreqCommand :: Command
   , runreqSource :: String
@@ -40,6 +75,8 @@ data RunRequest = RunRequest
   deriving (Show)
 
 -- | Body payload for a response to a 'RunRequest'.
+--
+-- JSON: object; either "err" or "ec", "sout", "serr", "timesecs".
 data RunResponse
   = RunResponseErr RunError
   | RunResponseOk
@@ -81,10 +118,25 @@ instance JSON RunError where
 
   showJSON RETimeOut = JSString (toJSString "timeout")
 
+instance JSON a => JSON (Message a) where
+  readJSON (JSObject obj) =
+    case mapM (`lookup` fromJSObject obj) ["sig", "pkey", "con"] of
+      Just [sig, pkey, con] ->
+        Message <$> readJSON sig
+                <*> readJSON pkey
+                <*> readJSON con
+      _ -> fail "Unable to read Message"
+  readJSON _ = fail "Unable to read Message"
+
+  showJSON (Message sig pkey con) =
+    JSObject $ toJSObject
+      [("sig", showJSON sig)
+      ,("pkey", showJSON pkey)
+      ,("con", showJSON con)]
+
 instance JSON RunRequest where
   readJSON (JSObject obj) =
-    case sequence (map (`lookup` fromJSObject obj)
-                       ["cmd", "src", "ver", "opt"]) of
+    case mapM (`lookup` fromJSObject obj) ["cmd", "src", "ver", "opt"] of
       Just [cmd, src, ver, opt] ->
         RunRequest <$> readJSON cmd
                    <*> (fromJSString <$> readJSON src)
@@ -105,8 +157,7 @@ instance JSON RunResponse where
     case lookup "err" (fromJSObject obj) of
       Just err -> RunResponseErr <$> readJSON err
       Nothing ->
-        case sequence (map (`lookup` fromJSObject obj)
-                           ["cmd", "src", "ver", "opt", "chal"]) of
+        case mapM (`lookup` fromJSObject obj) ["ec", "sout", "serr", "timesecs"] of
           Just [ec, sout, serr, timesecs] ->
             RunResponseOk <$> readJSON ec
                           <*> (fromJSString <$> readJSON sout)
@@ -123,6 +174,7 @@ instance JSON RunResponse where
       ,("serr", showJSON (toJSString serr))
       ,("timesecs", showJSON timesecs)]
 
+-- | Encoded as an integer, with 0 mapping to 'ExitSuccess'.
 instance JSON ExitCode where
   readJSON value = do
     code <- readJSON value :: Result Int
@@ -131,3 +183,67 @@ instance JSON ExitCode where
 
   showJSON ExitSuccess = showJSON (0 :: Int)
   showJSON (ExitFailure code) = showJSON code
+
+-- | Build and sign a message using your secret key.
+signMessage :: SigningBytes a => SecretKey -> a -> Message a
+signMessage skey content =
+  Message { sesmsgSignature = Sign.sign skey (signingBytes content)
+          , sesmsgPublicKey = Sign.publicKey skey
+          , sesmsgContent = content }
+
+class SigningBytes a where
+  -- | The result should be prefix-unambiguous: given a 'ByteString' that is
+  -- the result of 'signingBytes' plus some arbitrary content, it must be
+  -- unambiguous exactly what prefix corresponds to the type @a@, and what @a@
+  -- value it represents.
+  signingBytesB :: a -> Builder
+
+  -- | @'signingBytes' = 'Lazy.toStrict' . 'BSB.toLazyByteString' . 'signingBytesB'@
+  signingBytes :: a -> ByteString
+  signingBytes = Lazy.toStrict . BSB.toLazyByteString . signingBytesB
+
+instance SigningBytes ByteString where
+  signingBytesB bs = BSB.word64LE (fromIntegral @Int @Word64 (BS.length bs)) <> BSB.byteString bs
+
+newtype SigningBytesUTF8String = SigningBytesUTF8String String
+
+instance SigningBytes SigningBytesUTF8String where
+  signingBytesB (SigningBytesUTF8String s) =
+    BSB.word64LE (fromIntegral @Int @Word64 (length s)) <> BSB.stringUtf8 s
+
+instance SigningBytes Command where
+  signingBytesB CRun = BSB.word8 0
+  signingBytesB CCore = BSB.word8 1
+  signingBytesB CAsm = BSB.word8 2
+
+instance SigningBytes Optimisation where
+  signingBytesB O0 = BSB.word8 0
+  signingBytesB O1 = BSB.word8 1
+  signingBytesB O2 = BSB.word8 2
+
+deriving via SigningBytesUTF8String instance SigningBytes Version
+
+instance SigningBytes RunError where
+  signingBytesB RETimeOut = BSB.word8 0
+
+instance SigningBytes RunRequest where
+  signingBytesB (RunRequest cmd src ver opt) =
+    mconcat [signingBytesB cmd
+            ,signingBytesB (SigningBytesUTF8String src)
+            ,signingBytesB ver
+            ,signingBytesB opt]
+
+instance SigningBytes RunResponse where
+  signingBytesB (RunResponseErr err) =
+    mconcat [BSB.word8 0
+            ,signingBytesB err]
+  signingBytesB (RunResponseOk ec sout serr timesecs) =
+    mconcat [BSB.word8 1
+            ,signingBytesB ec
+            ,signingBytesB (SigningBytesUTF8String sout)
+            ,signingBytesB (SigningBytesUTF8String serr)
+            ,BSB.doubleLE timesecs]
+
+instance SigningBytes ExitCode where
+  signingBytesB ExitSuccess = BSB.int64LE 0
+  signingBytesB (ExitFailure ec) = BSB.int64LE (fromIntegral @Int @Int64 ec)
--- play-haskell-types/src/PlayHaskellTypes/Sign.hs
+++ play-haskell-types/src/PlayHaskellTypes/Sign.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 module PlayHaskellTypes.Sign (
   -- * Data types
   PublicKey(..),
@@ -16,8 +17,12 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Short as BSS
 import qualified Data.ByteArray as Mem
+import Data.Char (ord, chr)
+import Data.Word (Word8)
 import qualified Crypto.Error as Cr
 import qualified Crypto.PubKey.Ed25519 as Cr
+import Text.JSON (JSON)
+import qualified Text.JSON as JSON
 
 
 -- | 32 bytes.
@@ -54,10 +59,47 @@ sign :: SecretKey -> ByteString -> Signature
 sign (SecretKey skey) msg =
   Signature . BSS.pack . Mem.unpack $ Cr.sign skey (Cr.toPublic skey) msg
 
--- | The 'ByteString' is the message to sign.
+-- | The 'ByteString' is the message that was signed.
 verify :: PublicKey -> ByteString -> Signature -> Bool
 verify (PublicKey pkey) msg (Signature sgn) =
   case (,) <$> Cr.publicKey (BS.pack (BSS.unpack pkey))
            <*> Cr.signature (BS.pack (BSS.unpack sgn)) of
     Cr.CryptoPassed (pkey', sgn') -> Cr.verify pkey' msg sgn'
     Cr.CryptoFailed{} -> error "Invalid public key or signature given to 'verify'"
+
+instance JSON PublicKey where
+  readJSON (JSON.JSString (JSON.fromJSString -> s)) =
+    case hexDecode s of
+      Just sbs | BSS.length sbs == Cr.publicKeySize -> return (PublicKey sbs)
+      _ -> fail "Invalid hex string in PublicKey JSON"
+  readJSON _ = fail "Unable to read PublicKey"
+
+  showJSON (PublicKey sbs) = JSON.JSString (JSON.toJSString (hexEncode sbs))
+
+instance JSON Signature where
+  readJSON (JSON.JSString (JSON.fromJSString -> s)) =
+    case hexDecode s of
+      Just sbs | BSS.length sbs == Cr.signatureSize -> return (Signature sbs)
+      _ -> fail "Invalid hex string in Signature JSON"
+  readJSON _ = fail "Unable to read Signature"
+
+  showJSON (Signature sbs) = JSON.JSString (JSON.toJSString (hexEncode sbs))
+
+hexDecode :: String -> Maybe BSS.ShortByteString
+hexDecode s = BSS.pack <$> go s
+  where
+    go "" = Just []
+    go (c1:c2:cs) = (:) <$> ((+) <$> digit c1 <*> digit c2) <*> go cs
+    go _ = Nothing
+
+    digit :: Char -> Maybe Word8
+    digit c | ord '0' <= ord c, ord c <= ord '9' = Just $ fromIntegral $ ord c - ord '0'
+            | ord 'a' <= ord c, ord c <= ord 'f' = Just $ fromIntegral $ ord c - ord 'a' + 10
+            | ord 'A' <= ord c, ord c <= ord 'F' = Just $ fromIntegral $ ord c - ord 'A' + 10
+            | otherwise = Nothing
+
+hexEncode :: BSS.ShortByteString -> String
+hexEncode = concatMap go . BSS.unpack
+  where go n = [digit (n `div` 16), digit (n `mod` 16)]
+        digit n | n < 10 = chr (ord '0' + fromIntegral n)
+                | otherwise = chr (ord 'a' + fromIntegral n - 10)
