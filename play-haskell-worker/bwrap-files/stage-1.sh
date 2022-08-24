52ec674ad28f11b07dac8f5f339189b2ef2426d4
Tom Smeding <tom@tomsmeding.com>
2022-08-24 21:53:13 +0200

    systemd-run --user

--- play-haskell-worker/bwrap-files/stage-1.sh
+++ play-haskell-worker/bwrap-files/stage-1.sh
@@ -14,6 +14,15 @@ if [[ $# -ne 1 ]]; then
 fi
 ghc_out_fd="$1"
 
+# Close all open file descriptors other than 0,1,2 and the ghc output FD
+close_cmdline="exec"
+for fd in $(ls /proc/$$/fd); do
+  if [[ "$fd" -gt 2 && "$fd" -ne "$ghc_out_fd" ]]; then
+    close_cmdline="$close_cmdline $fd>&-"
+  fi
+done
+eval "$close_cmdline"
+
 tmpdir=$(mktemp -d)
 
 trap "rm -r '$tmpdir'" EXIT
@@ -21,6 +30,25 @@ trap "rm -r '$tmpdir'" EXIT
 mkfifo "${tmpdir}/ghc-out"
 # This cat will exit automatically once the stage-2 exits (closes the fifo).
 ( cat <"${tmpdir}/ghc-out" >&"$ghc_out_fd" ) &
-ghc_out_catpid=$?
+ghc_out_catpid=$!
 
-./systemd-run-shim ./stage-2.sh "${tmpdir}/ghc-out"
+args=(
+  --user
+  --description='play-haskell-worker cpuquota'
+  --pipe
+  --wait
+  --collect
+  --same-dir
+  --service-type=exec
+  --setenv=PATH="$PATH"
+  --quiet
+  --property=CPUQuota=100%
+  # Limit memory to 600 MiB. Note that the compiled program gets a 500 MiB memory
+  # limit via the GHC RTS, so this limit is 1. to constrain GHC itself (including
+  # any TH code), and 2. as a second-layer defense.
+  --property=MemoryMax=600M
+  --property=TasksMax=50
+  --property=LimitCORE=0
+)
+
+systemd-run "${args[@]}" -- ./stage-2.sh "${tmpdir}/ghc-out"
