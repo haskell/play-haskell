c0341e70b083961b7ecb0aa7e051e620a3e16135
Tom Smeding <tom@tomsmeding.com>
2023-02-27 21:40:59 +0100

    Up default GHC to 9.2.7

--- play-haskell-server/static/play-index.ts
+++ play-haskell-server/static/play-index.ts
@@ -119,7 +119,7 @@ type Runner = "run" | "core" | "asm";
 
 let lastRunKind: Runner = "run";
 
-const defaultGHCversion: string = "9.2.5";
+const defaultGHCversion: string = "9.2.7";
 
 
 function performXHR(
