9978b6dcb58bd308e68eb5d29336c8a915b6b203
Tom Smeding <tom@tomsmeding.com>
2023-04-19 23:03:27 +0200

    worker: Switch to ubuntu 22.04 for sandbox image

--- play-haskell-worker/bwrap-files/chroot-initialise.sh
+++ play-haskell-worker/bwrap-files/chroot-initialise.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#set -e
 sed -i '/^_apt:/d' /etc/passwd  # See https://github.com/containers/bubblewrap/issues/210
-apt update && apt install -y build-essential curl libffi-dev libffi7 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 locales zlib1g-dev gawk
+apt update
+apt install -y dialog  # apt needs this to show stuff, so let's install it first
+apt upgrade -y
+apt install -y build-essential curl libffi-dev libffi7 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 locales zlib1g-dev gawk
 PATH="$PATH:/usr/sbin" dpkg-reconfigure locales
