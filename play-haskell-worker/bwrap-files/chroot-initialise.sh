bea332455088f074e05dd86bbdd88f9dc8ef9a2c
Tom Smeding <tom@tomsmeding.com>
2023-04-20 01:02:50 +0200

    chroot-initialise: Remove _apt user again after apt upgrade

--- play-haskell-worker/bwrap-files/chroot-initialise.sh
+++ play-haskell-worker/bwrap-files/chroot-initialise.sh
@@ -4,5 +4,6 @@ sed -i '/^_apt:/d' /etc/passwd  # See https://github.com/containers/bubblewrap/i
 apt update
 apt install -y dialog  # apt needs this to show stuff, so let's install it first
 apt upgrade -y
+sed -i '/^_apt:/d' /etc/passwd
 apt install -y build-essential curl libffi-dev libffi7 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 locales zlib1g-dev gawk
 PATH="$PATH:/usr/sbin" dpkg-reconfigure locales
