40ffdfd221f8ee71554df656a904e3d2ac1244d0
Tom Smeding <tom@tomsmeding.com>
2024-08-26 23:17:41 +0200

    More WIP statistics

--- play-haskell-server/src/Statistics.hs
+++ play-haskell-server/src/Statistics.hs
@@ -1,6 +1,56 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
 module Statistics where
+
+import Control.Concurrent
+import Control.Monad (when)
+import Data.IORef
+import qualified Data.Time as DT
+import qualified Data.Time.Clock.POSIX as DT
 
 import Statistics.Stats
 
 
+rotateTime :: DT.TimeOfDay
+rotateTime = DT.TimeOfDay 5 0 0
 
+data StData = StData
+  { stdaJobTime :: Stats ('StatOptions True True True) Double
+  , stdaQueueLength :: Stats ('StatOptions True True True) Double }
+
+emptyStData :: StData
+emptyStData = StData (statsEmpty 10) (statsEmpty 10)
+
+newtype Statistics = Statistics (IORef StData)
+
+startStatistics :: IO Statistics
+startStatistics = do
+  ref <- newIORef emptyStData
+  _ <- forkIO $ workerThread ref
+  return (Statistics ref)
+
+rotateStats :: IORef StData -> IO ()
+rotateStats ref = do
+  _
+
+workerThread :: IORef StData -> IO ()
+workerThread ref = do
+  now <- DT.getCurrentTime
+  zone <- DT.getTimeZone now
+  let DT.LocalTime day _timeofday = DT.utcToLocalTime zone now
+  let waitTill =
+        let candidate@(DT.UTCTime utcday utctime) =
+              DT.localTimeToUTC zone (DT.LocalTime day rotateTime)
+        in if candidate <= now then DT.UTCTime (succ utcday) utctime
+                               else candidate
+
+  -- converting to posix time here is not quite correct, but I don't care
+  let waitTime = realToFrac (DT.utcTimeToPOSIXSeconds waitTill) - realToFrac (DT.utcTimeToPOSIXSeconds now) :: Double
+
+  when (fromIntegral @Int @Integer maxBound < 24*3600*1000*1000) $ error "This doesn't work on 32-bit systems"
+
+  threadDelay (round (waitTime * 1000*1000))
+
+  rotateStats ref
+
+  workerThread ref
--- play-haskell-server/src/Statistics/Stats.hs
+++ play-haskell-server/src/Statistics/Stats.hs
@@ -1,25 +1,38 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE BangPatterns #-}
 module Statistics.Stats (
+  -- * Statistics collector
   Stats,
-  oneStat,
+  StatOptions(..),
+  statsEmpty,
+  statsAdd,
+
+  -- * Getting results
   statsCount,
   statsMean,
+  statsMean',
   statsStddev,
   statsMin,
   statsMax,
+  statsHist,
+
+  -- * Pretty-printing stats
+  prettyStats,
 ) where
 
-import qualified Data.Map.Strict as Map
-import Data.Map.Strict (Map)
+import Data.List (transpose)
+import qualified Data.IntMap.Strict as IMap
+import Data.IntMap.Strict (IntMap)
+import Data.Kind (Constraint)
+import Data.Maybe (fromMaybe)
 import Data.Monoid
 import Data.Semigroup
 
@@ -36,7 +49,8 @@ type family If a b c where
   If True b c = b
   If False b c = c
 
-mkIf :: forall b c a. SBool a -> b -> c -> If a b c
+-- | This type is ambiguous, use type applications.
+mkIf :: forall b c a. SBool a -> (a ~ True => b) -> (a ~ False => c) -> If a b c
 mkIf STrue x _ = x
 mkIf SFalse _ y = y
 
@@ -63,21 +77,22 @@ data RunningHist a
   = RunningHist !Int                   -- ^ target number of bins
                 !Int                   -- ^ actual number of bins
                 !(Maybe' (Pair' a a))  -- ^ (min, max)
-                !(Map Int Int)         -- ^ the bins
+                !(IntMap Int)          -- ^ the bins
+  deriving (Show)
 
 rhistInit :: Int -> RunningHist a
 rhistInit n = RunningHist n 0 Nothing' mempty
 
-rhistAdd :: forall a. (RealFrac a, Ord a) => a -> RunningHist a -> RunningHist a
+rhistAdd :: forall a. RealFrac a => a -> RunningHist a -> RunningHist a
 rhistAdd !x (RunningHist targetn _ Nothing' _) =
-  RunningHist targetn 1 (Just' (Pair' x x)) (Map.singleton 0 1)
+  RunningHist targetn 1 (Just' (Pair' x x)) (IMap.singleton 0 1)
 rhistAdd x (RunningHist targetn actualn bds@(Just' (Pair' mi ma)) mp)
   | mi <= x, x < ma =
       let idx | ma <= mi = 0
               | otherwise = min (actualn - 1)  -- just to be sure...
                                 (floor ((x - mi) * fromIntegral actualn / (ma - mi)))
       in RunningHist targetn actualn bds
-                     (Map.insertWith (+) idx 1 mp)
+                     (IMap.insertWith (+) idx 1 mp)
   | otherwise =
       let currange = ma - mi
           newrange = max ma x - min mi x
@@ -88,32 +103,40 @@ rhistAdd x (RunningHist targetn actualn bds@(Just' (Pair' mi ma)) mp)
            then -- put all existing data in one bin
              if x < mi
                then RunningHist targetn targetn (Just' (Pair' x ma))
-                                (Map.fromList [(0, 1), (targetn - 1, sum (Map.elems mp))])
+                                (IMap.fromList [(0, 1), (targetn - 1, sum (IMap.elems mp))])
                else -- Make x the _start_ of the last bin, because the histogram's range is [min, max)
                     let newma = x + (x - mi) / fromIntegral (targetn - 1)
                     in RunningHist targetn targetn (Just' (Pair' mi newma))
-                                   (Map.fromList [(0, sum (Map.elems mp)), (targetn - 1, 1)])
+                                   (IMap.fromList [(0, sum (IMap.elems mp)), (targetn - 1, 1)])
 
-
-           -- If the new range would yield at least (roughly) twice the target
-           -- number of bins, redistribute.
-           -- Because the indices are not too large (<= targetn^2), so we can
+           -- Redistribute. Ensure that the number of bins stays below
+           -- 2*targetn.
+           -- Because the indices are not too large (<= targetn^2), we can
            -- sensibly compute with the bin width here.
            else
              let binwid = (ma - mi) / fromIntegral actualn
                  nbefore = ceiling ((mi - min mi x) / binwid)
                  mi' = mi - fromIntegral nbefore * binwid
                  nafter | x < ma = 0
-                        | otherwise = floor ((max ma x - ma) / binwid + 1)
+                        | otherwise =
+                            let candidate = floor ((max ma x - ma) / binwid + 1)
+                            in if ma + fromIntegral candidate * binwid <= x
+                                 then candidate + 1 else candidate  -- catch rounding errors
                  ma' = ma + fromIntegral nafter * binwid
-                 newnbins = nbefore + actualn + nafter
-                 reduction = newnbins `quot` targetn
-                 idx = floor ((x - mi') / (binwid / fromIntegral reduction))
+                 newnbinsPrecise = nbefore + actualn + nafter
+                 reduction = newnbinsPrecise `quot` targetn
+                 newnbins = (newnbinsPrecise + reduction - 1) `quot` reduction
+                 idx = floor ((x - mi') / binwid) `quot` reduction
              in RunningHist targetn
                             newnbins
                             (Just' (Pair' mi' ma'))
-                            (Map.insertWith (+) idx 1
-                              (Map.mapKeysWith (+) (\i -> (i + nbefore) `quot` reduction) mp))
+                            (IMap.insertWith (+) idx 1
+                              (IMap.mapKeysWith (+) (\i -> (i + nbefore) `quot` reduction) mp))
+
+rhistFinish :: RunningHist a -> Maybe (a, a, [Int])
+rhistFinish (RunningHist _ _ Nothing' _) = Nothing
+rhistFinish (RunningHist _ actualn (Just' (Pair' mi ma)) mp) =
+  Just (mi, ma, [fromMaybe 0 (IMap.lookup i mp) | i <- [0 .. actualn-1]])
 
 
 data StatOptions = StatOptions Bool Bool Bool
@@ -128,35 +151,34 @@ data Stats (op :: StatOptions) a = Stats
   { statsSum :: !(If (StatOpMean op) (Sum a) ())
   , statsSumSq :: !(If (StatOpStddev op) (Sum a) ())
   , statsCount :: !(Sum Int)
-  , statsMin :: !(Maybe' (Min a))
-  , statsMax :: !(Maybe' (Max a)) }
+  , statsMin' :: !(Maybe' (Min a))
+  , statsMax' :: !(Maybe' (Max a))
+  , statsHist' :: !(If (StatOpHist op) (RunningHist a) ()) }
 
-instance (Num a, Ord a, KnownStatOptions op) => Semigroup (Stats op a) where
-  Stats sx sx2 n mi ma <> Stats sx' sx2' n' mi' ma' =
-    Stats (sappendIf @(Sum a) @() (knownBool @(StatOpMean op)) sx sx')
-          (sappendIf @(Sum a) @() (knownBool @(StatOpStddev op)) sx2 sx2')
-          (n <> n') (mi <> mi') (ma <> ma')
-    where
-      -- This type is ambiguous; use type applications.
-      sappendIf :: forall b c a'. (Semigroup b, Semigroup c) => SBool a' -> If a' b c -> If a' b c -> If a' b c
-      sappendIf STrue x y = x <> y
-      sappendIf SFalse x y = x <> y
+statsEmpty :: forall op a. (Num a, KnownStatOptions op) => If (StatOpHist op) Int () -> Stats op a
+statsEmpty histsize =
+  Stats (mkIf @(Sum a) @() (knownBool @(StatOpMean op)) (Sum 0) ())
+        (mkIf @(Sum a) @() (knownBool @(StatOpStddev op)) (Sum 0) ())
+        0
+        Nothing' Nothing'
+        (mkIf @(RunningHist a) @() (knownBool @(StatOpHist op)) (rhistInit histsize) ())
 
-instance (Num a, Ord a, KnownStatOptions op) => Monoid (Stats op a) where
-  mempty = Stats (mkIf @(Sum a) (knownBool @(StatOpMean op)) (Sum 0) ())
-                 (mkIf @(Sum a) (knownBool @(StatOpStddev op)) (Sum 0) ())
-                 0 Nothing' Nothing'
+statsAdd :: forall a op.
+            (Num a, Ord a, KnownStatOptions op, If (StatOpHist op) (RealFrac a) (() :: Constraint))
+         => a -> Stats op a -> Stats op a
+statsAdd x s = Stats
+  { statsSum = mkIf @(Sum a) @() (knownBool @(StatOpMean op)) (Sum x <> statsSum s) ()
+  , statsSumSq = mkIf @(Sum a) @() (knownBool @(StatOpStddev op)) (Sum (x * x) <> statsSumSq s) ()
+  , statsCount = 1 <> statsCount s
+  , statsMin' = Just' (Min x) <> statsMin' s
+  , statsMax' = Just' (Max x) <> statsMax' s
+  , statsHist' = mkIf @(RunningHist a) @() (knownBool @(StatOpHist op)) (rhistAdd x (statsHist' s)) () }
 
-oneStat :: forall a op. (Num a, KnownStatOptions op) => a -> Stats op a
-oneStat x = Stats
-  { statsSum = mkIf (knownBool @(StatOpMean op)) (Sum x) ()
-  , statsSumSq = mkIf (knownBool @(StatOpStddev op)) (Sum (x * x)) ()
-  , statsCount = 1
-  , statsMin = Just' (Min x)
-  , statsMax = Just' (Max x) }
+statsMean' :: (Fractional b, StatOpMean op ~ True) => (a -> b) -> Stats op a -> b
+statsMean' f s = f (getSum (statsSum s)) / fromIntegral (getSum (statsCount s))
 
 statsMean :: (Fractional a, StatOpMean op ~ True) => Stats op a -> a
-statsMean s = getSum (statsSum s) / fromIntegral (getSum (statsCount s))
+statsMean = statsMean' id
 
 -- SampleVar(X)
 --   = Var(X) * BesselCorrection(n)
@@ -171,3 +193,53 @@ statsStddev s =
       sumsq = getSum (statsSumSq s)
       n = getSum (statsCount s)
   in sqrt $ sumsq / fromIntegral (n - 1) - sumx ^ (2::Int) / fromIntegral (n * (n - 1))
+
+-- | Returns 'Nothing' if dataset is empty.
+statsMin :: Stats op a -> Maybe a
+statsMin s = case statsMin' s of
+               Nothing' -> Nothing
+               Just' (Min x) -> Just x
+
+-- | Returns 'Nothing' if dataset is empty.
+statsMax :: Stats op a -> Maybe a
+statsMax s = case statsMax' s of
+               Nothing' -> Nothing
+               Just' (Max x) -> Just x
+
+-- | Returns 'Nothing' if there is no data, and hence nothing to base histogram
+-- bounds on.
+--
+-- If there is data, returns lower limit of first bar, upper limit of last bar,
+-- and the counts of the bars in between. The bar separators are evenly spaced,
+-- and a bar's range is inclusive-exclusive.
+statsHist :: StatOpHist op ~ True => Stats op a -> Maybe (a, a, [Int])
+statsHist s = rhistFinish (statsHist' s)
+
+prettyStats :: forall op a. (Show a, Floating a, KnownStatOptions op) => Stats op a -> String
+prettyStats s =
+  (case knownBool @(StatOpMean op) of
+     STrue -> "Mean: " ++ show (statsMean s)
+              ++ (case knownBool @(StatOpStddev op) of
+                    STrue -> " ± " ++ show (statsStddev s)
+                    SFalse -> "")
+              ++ "\n"
+     SFalse -> "")
+  ++ "Range: [" ++ show (statsMin s) ++ ", " ++ show (statsMax s) ++ "]\n"
+  ++ (case knownBool @(StatOpHist op) of
+        STrue
+          | Just (mi, ma, l) <- statsHist s ->
+              let sMi = show mi
+                  prelen = length sMi
+                  height = 4
+                  maxval = maximum l
+                  table = transpose
+                    [if val > 0 && val * (height * 8) `quot` maxval == 0
+                       then replicate (height - 1) ' ' ++ "."
+                       else [" ▁▂▃▄▅▆▇█" !! max 0 (min 8 (val * (height * 8) `quot` maxval - 8 * i))
+                            | i <- [height-1, height-2 .. 0]]
+                    | val <- l]
+              in "Histogram: " ++ show (statsHist' s) ++ "\n"
+                 ++ unlines ([replicate prelen ' ' ++ " [" ++ r ++ "]" | r <- init table]
+                             ++ [sMi ++ " [" ++ last table ++ "] " ++ show ma])
+          | otherwise -> "Histogram: <empty dataset>\n"
+        SFalse -> "")
