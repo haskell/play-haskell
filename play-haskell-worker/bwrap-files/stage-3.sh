646263d5d9d9fa4964052f00a00c17e5e316f721
Tom Smeding <tom@tomsmeding.com>
2025-01-19 21:16:17 +0100

    stage-3: Indent using spaces

--- play-haskell-worker/bwrap-files/stage-3.sh
+++ play-haskell-worker/bwrap-files/stage-3.sh
@@ -17,38 +17,38 @@ IFS="" read -r version
 
 # Check that there are only version-like characters
 if [[ "${version//[0-9.alpha-]/}" != "" ]]; then
-	echo >&2 "Invalid version"
-	exit 1
+  echo >&2 "Invalid version"
+  exit 1
 fi
 
 if [[ ! -f /builders/build-"${version}".sh ]]; then
-	echo >&2 "Version ${version} not available"
-	exit 1
+  echo >&2 "Version ${version} not available"
+  exit 1
 fi
 
 case "$command" in
-	run)
-		cat >Main.hs
-		/builders/build-"${version}".sh "${opt}" Main.hs >/tmp/null 2>&"$ghc_out_fd"
-		if [[ -f Main ]]; then
-			./Main +RTS -M500m -RTS
-		else
-			echo >&2 "Compilation succeeded, but no executable was produced. Perhaps your module is not called 'Main'?"
-		fi
-		;;
-	core)
-		cat >input.hs
-		/builders/build-"${version}".sh -ddump-simpl -ddump-to-file "${opt}" input.hs >/tmp/null 2>&"$ghc_out_fd"
-		cat input.dump-simpl
-		;;
-	asm)
-		cat >input.hs
-		/builders/build-"${version}".sh -ddump-asm -ddump-to-file "${opt}" input.hs >/tmp/null 2>&"$ghc_out_fd"
-		cat input.dump-asm
-		;;
+  run)
+    cat >Main.hs
+    /builders/build-"${version}".sh "${opt}" Main.hs >/tmp/null 2>&"$ghc_out_fd"
+    if [[ -f Main ]]; then
+      ./Main +RTS -M500m -RTS
+    else
+      echo >&2 "Compilation succeeded, but no executable was produced. Perhaps your module is not called 'Main'?"
+    fi
+    ;;
+  core)
+    cat >input.hs
+    /builders/build-"${version}".sh -ddump-simpl -ddump-to-file "${opt}" input.hs >/tmp/null 2>&"$ghc_out_fd"
+    cat input.dump-simpl
+    ;;
+  asm)
+    cat >input.hs
+    /builders/build-"${version}".sh -ddump-asm -ddump-to-file "${opt}" input.hs >/tmp/null 2>&"$ghc_out_fd"
+    cat input.dump-asm
+    ;;
 
-	*)
-		echo >&2 "Unknown command"
-		exit 1
-		;;
+  *)
+    echo >&2 "Unknown command"
+    exit 1
+    ;;
 esac
